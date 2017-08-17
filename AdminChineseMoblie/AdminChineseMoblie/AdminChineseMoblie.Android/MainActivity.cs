91ae0fe83420953a039a248ceaf1df9384ce1e0a
volodymyr <vovka199856@gmail.com>
2017-08-17 15:55:08 +0300

    changed name and icon of AdminMobile

--- AdminChineseMoblie/AdminChineseMoblie/AdminChineseMoblie.Android/MainActivity.cs
+++ AdminChineseMoblie/AdminChineseMoblie/AdminChineseMoblie.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace AdminChineseMoblie.Droid
 {
-    [Activity(Label = "AdminChineseMoblie", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Chinese's Admin", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
